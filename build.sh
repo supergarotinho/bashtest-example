975b0886357e48f5b9bc9e880738ed059affbcb4
Anderson Roberto Santos dos Santos <anderson@gruponeuro.com.br>
2018-02-13 16:26:47 -0200

    Creating build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Measure time
 start=$SECONDS
@@ -6,58 +6,62 @@ start=$SECONDS
 # Add color if not
 export TERM=xterm-256color
 # Logging stuff.
-function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
-function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
-function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
-function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
+function e_header()   { echo -e "\\n\\033[1m$*\\033[0m"; }
+function e_success()  { echo -e " \\033[1;32m✔\\033[0m  $*"; }
+function e_error()    { echo -e " \\033[1;31m✖\\033[0m  $*"; }
+function e_arrow()    { echo -e " \\033[1;34m➜\\033[0m  $*"; }
 
 # Removes the coverage folder
 rm -rf test/coverage 2>/dev/null
-mkdir test/coverage
+mkdir -p test/coverage
 
 failed=false
 
-kcov="kcov --exclude-path=/root/shunit2,/source/build.sh,/source/test"
-coverage_dir="$( dirname "${BASH_SOURCE[0]}")/test/coverage"
-kcov_cmd="${kcov} ${coverage_dir}"
+kcov_cmd=(kcov "--exclude-path=/root/shunit2,/source/build.sh,/source/test")
+actual_dir=$( dirname "${BASH_SOURCE[0]}")
+coverage_dir="${actual_dir}/test/coverage"
+kcov_cmd+=("${coverage_dir}")
 
 e_header "Running unit tests"
-# Running unit tests
-e_arrow "Running: ./test/unit-tests.sh"
-docker run --rm \
-  --security-opt seccomp=unconfined \
-  -v $(pwd):/source \
-  -e "TERM=xterm-256color" supergarotinho/bashtest ./test/unit-tests.sh
-result=$?
-[ $result -eq 0 ] && e_success "./test/unit-tests.sh passed!"
-[ ! $result -eq 0 ] && e_error "./test/unit-tests.sh failed!" && failed=true
-
 for file in ./test/*unit-tests.sh
 do
-  echo ${file}
+    # Running unit tests
+    e_arrow "Running: ${file}"
+    docker run --rm \
+      --security-opt seccomp=unconfined \
+      -v "$(pwd)":/source \
+      -e "TERM=xterm-256color" supergarotinho/bashtest "${kcov_cmd[@]}" "${file}"
+    result=$?
+    [ $result -eq 0 ] && e_success "${file} passed!"
+    [ ! $result -eq 0 ] && e_error "${file} failed!" && failed=true
 done
 
-for file in ./test/*functional-tests.sh
-do
-  echo ${file}
-done
+if [[ ${failed} == "false" ]]
+then
+    e_header "Running functional tests"
+    for file in ./test/*functional-tests.sh
+    do
+        e_arrow "${file}"
+    done
+fi
 
 e_header "Running spellcheck..."
 for file in ./*.sh
 do
-  e_arrow "Running spellcheck on: ${file}"
-  docker run -v "$PWD:/mnt" koalaman/shellcheck ${file}
-  result=$?
-  [ $result -eq 0 ] && e_success "${file} passed!"
-  [ ! $result -eq 0 ] && e_error "${file} failed!" && failed=true
+    e_arrow "Running spellcheck on: ${file}"
+    docker run -v "$(pwd)":/mnt koalaman/shellcheck "${file}"
+    result=$?
+    [ $result -eq 0 ] && e_success "${file} passed!"
+    [ ! $result -eq 0 ] && e_error "${file} failed!" && failed=true
 done
 
 # TO generate a json and then put somewhere in the build
-# shellcheck -f json myscript myotherscript
+
+#  -f json myscript myotherscript
 
 total=$((SECONDS - start))
 minutes=$((total / 60))
 seconds=$((total % 60))
-printf "%d minutes and %02d seconds" ${minutes} ${seconds}
+printf "%d minutes and %02d seconds\\n" ${minutes} ${seconds}
 
 [ ${failed} == "true" ] && exit 1
