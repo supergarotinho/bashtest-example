f82d2694e5566b910edb60ee9d5a6caab49adc11
Anderson Santos <anderson@gruponeuro.com.br>
2018-03-02 18:33:46 -0300

    Parameter combination testing

--- test/functional-tests.sh
+++ test/functional-tests.sh
@@ -48,6 +48,38 @@ testWithDir()
     assertTrue "Expected 0, but got ${rtrn}." "[ ${rtrn} -eq 0 ]"
 }
 
+testWithHelp()
+{
+    ${script} --help >/dev/null
+    rtrn=$?
+
+    assertTrue "Expected 0, but got ${rtrn}." "[ ${rtrn} -eq 0 ]"
+}
+
+testParameterCombinations() {
+    while read -r description with_dir with_space_args with_boolean_arg ok; do
+        local arguments=()
+        [[ ${with_dir} -eq ${SHUNIT_TRUE} ]] && arguments+=("-d") && arguments+=("./directory")
+        [[ ${with_space_args} -eq ${SHUNIT_TRUE} ]] && arguments+=("-s") && arguments+=("space arg")
+        [[ ${with_boolean_arg} -eq ${SHUNIT_TRUE} ]] && arguments+=("-b")
+
+        ${script} "${arguments[@]}" > /dev/null
+        local return=$?
+
+        # Teste se o retorno bate com o retorno esperado para o cenário
+        assertEquals "$description: expected ${ok}. Received: ${return}" "${ok}" "${return}"
+    done << EOF
+With_dir_and_boolean ${SHUNIT_TRUE} ${SHUNIT_FALSE} ${SHUNIT_TRUE} 0
+With_dir_and_space ${SHUNIT_TRUE} ${SHUNIT_TRUE} ${SHUNIT_FALSE} 0
+Without_dir_and_boolean ${SHUNIT_FALSE} ${SHUNIT_FALSE} ${SHUNIT_TRUE} 1
+Without_dir_and_space ${SHUNIT_FALSE} ${SHUNIT_TRUE} ${SHUNIT_FALSE} 1
+Without_dir_and_wiht_space_and_boolean ${SHUNIT_FALSE} ${SHUNIT_TRUE} ${SHUNIT_TRUE} 1
+EOF
+    # limpa as variáveis locais
+    #unset fonte extensoes extensoes_teste
+}
+
+
 # load and run shUnit2
 #[ -n "${ZSH_VERSION:-}" ] && SHUNIT_PARENT=$0
 . /root/shunit2/shunit2
